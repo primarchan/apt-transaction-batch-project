d2f022c6982d37ac4303d8a4dfd5da4a21074cf0
An T.G <boltcarrier@naver.com>
2023-10-11 22:45:03 +0900

    동 Batch - Job, Step, FlatFileItemReader 구현

--- src/main/java/com/example/housebatch/HouseBatchApplication.java
+++ src/main/java/com/example/housebatch/HouseBatchApplication.java
@@ -1,9 +1,11 @@
 package com.example.housebatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class HouseBatchApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/housebatch/job/lawd/LawdInsertJobConfig.java
@@ -0,0 +1,78 @@
+package com.example.housebatch.job.lawd;
+
+import com.example.housebatch.core.entity.Lawd;
+import com.example.housebatch.job.validator.FilePathParameterValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import static com.example.housebatch.job.lawd.LawdFieldSetMapper.*;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class LawdInsertJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job lawdInsertJob(Step lawdInsertStep) {
+        return jobBuilderFactory.get("lawdInsertJob")
+                .incrementer(new RunIdIncrementer())
+                .validator(new FilePathParameterValidator())
+                .start(lawdInsertStep)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step lawdInsertStep(
+            FlatFileItemReader<Lawd> lawdFileItemReader,
+            ItemWriter<Lawd> lawdItemWriter
+    ) {
+        return stepBuilderFactory.get("lawdInsertStep")
+                .<Lawd, Lawd>chunk(1000)
+                .reader(lawdFileItemReader)
+                .writer(lawdItemWriter)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<Lawd> lawdFileItemReader(
+            @Value("#{jobParameters['filePath']}") String filePath
+    ) {
+
+        return new FlatFileItemReaderBuilder<Lawd>().name("lawdFileItemReader")
+                .delimited()
+                .delimiter("\t")
+                .names(LAWD_CD, LAWD_DONG, EXIST)
+                .linesToSkip(1)
+                .fieldSetMapper(new LawdFieldSetMapper())
+                .resource(new ClassPathResource(filePath))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<Lawd> lawdItemWriter() {
+
+        return items -> items.forEach(System.out::println);
+    }
+
+}
