f3f950c495ad4b3bb64efecbd9cc7cf93f7835fe
An T.G <boltcarrier@naver.com>
2023-10-13 21:14:02 +0900

    실거래가 Batch 개발 - JobParameter 로 API 호출

--- src/main/java/com/example/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/example/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -3,14 +3,18 @@ package com.example.housebatch.job.apt;
 import com.example.housebatch.Adapter.ApartmentApiResource;
 import com.example.housebatch.core.dto.AptDealDto;
 import com.example.housebatch.job.validator.FilePathParameterValidator;
+import com.example.housebatch.job.validator.LawdCdParameterValidator;
+import com.example.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
@@ -22,6 +26,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
+import java.util.Arrays;
 
 @Slf4j
 @Configuration
@@ -38,8 +43,19 @@ public class AptDealInsertJobConfig {
     public Job aptDealInsertJob(Step aptDealInsertStep) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
+                .validator(aptDealJobParameterValidator())
                 .start(aptDealInsertStep)
                 .build();
+    }
+
+    private JobParametersValidator aptDealJobParameterValidator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+        validator.setValidators(Arrays.asList(
+                new YearMonthParameterValidator(),
+                new LawdCdParameterValidator()
+        ));
+
+        return validator;
     }
 
     @Bean
@@ -58,10 +74,12 @@ public class AptDealInsertJobConfig {
     @Bean
     @StepScope
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
+            @Value("#{jobParameters['lawdCd']}") String lawdCd,
+            @Value("#{jobParameters['yearMonth']}") String yearMonth,
             Jaxb2Marshaller aptDealDtoMarshaller
     ) {
         return new StaxEventItemReaderBuilder<AptDealDto>().name("aptDealResourceReader")
-                .resource(apartmentApiResource.getResource("41135", YearMonth.of(2021, 7)))
+                .resource(apartmentApiResource.getResource(lawdCd, YearMonth.parse(yearMonth)))
                 .addFragmentRootElements("item")
                 .unmarshaller(aptDealDtoMarshaller)
                 .build();
---
+++ src/main/java/com/example/housebatch/job/validator/YearMonthParameterValidator.java
@@ -0,0 +1,32 @@
+package com.example.housebatch.job.validator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.util.StringUtils;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeParseException;
+
+@Slf4j
+public class YearMonthParameterValidator implements JobParametersValidator {
+
+    private static final String YEAR_MONTH = "yearMonth";
+
+    @Override
+    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+            String yearMonth = parameters.getString(YEAR_MONTH);
+
+            if (!StringUtils.hasText(yearMonth)) {
+                throw new JobParametersInvalidException(YEAR_MONTH + " 가 빈 문자열이거나 존재하지 않습니다.");
+            }
+
+            try {
+                YearMonth.parse(yearMonth);
+            } catch (DateTimeParseException e) {
+                throw new JobParametersInvalidException(YEAR_MONTH + " 가 올바른 날짜 형식이 아닙니다. yyyy-MM 형식이어야 합니다.");
+            }
+    }
+
+}
