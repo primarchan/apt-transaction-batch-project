680145630bb0c0333ce6bee86586dccebb715ded
An T.G <boltcarrier@naver.com>
2023-10-14 23:12:20 +0900

    Batch 테스트 - 동 Batch, 실거래가 Batch, 알림 Batch 테스트 구현

--- src/main/java/com/example/housebatch/adapter/SendServiceImpl.java
+++ src/main/java/com/example/housebatch/adapter/SendServiceImpl.java
@@ -3,6 +3,9 @@ package com.example.housebatch.adapter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * TODO : 실제 이메일 전송 로직 구현 필요
+ */
 @Slf4j
 @Service
 public class SendServiceImpl implements SendService {
